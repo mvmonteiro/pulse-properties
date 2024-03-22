77d41db54209d87f8c8a20491dc7f2dce94827ae
mvmonteiro <48728730+mvmonteiro@users.noreply.github.com>
2024-03-21 21:10:26 -0300

    feat: details for the property page with dynamic data

--- project/utils/services/requests.js
+++ project/utils/services/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// fetch all properties
 async function fetchProperties() {
     try {
         // handle the case where the domain is not available yet
@@ -20,4 +21,25 @@ async function fetchProperties() {
     }
 }
 
-export { fetchProperties }
+// fetch single property
+async function fetchProperty(id) {
+    try {
+        // handle the case where the domain is not available yet
+        if (!apiDomain) {
+            return null
+        }
+
+        const res = await fetch(`${apiDomain}/properties/${id}`)
+
+        if (!res.ok)
+            throw new Error('Failed to fetch data')
+
+        return await res.json()
+    }
+    catch (err) {
+        console.log(err)
+        return null
+    }
+}
+
+export { fetchProperties, fetchProperty }
